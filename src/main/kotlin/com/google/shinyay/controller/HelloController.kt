d429cbf72cc5cf8936d61eebf8ba30d74c6766e2
shinyay <shinya.com@gmail.com>
2021-01-24 22:59:37 +0900

    Add DateTimeFormat

--- src/main/kotlin/com/google/shinyay/controller/HelloController.kt
+++ src/main/kotlin/com/google/shinyay/controller/HelloController.kt
@@ -1,9 +1,16 @@
 package com.google.shinyay.controller
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.format.DateTimeFormatter
 
 @RestController
 @RequestMapping("/api/v1")
 class HelloController {
+
+    @GetMapping("/hello")
+    fun hello() {
+        val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
+    }
 }
